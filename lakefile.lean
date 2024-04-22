2f19d68eda45cce1da782bfc547c39510a72ba89
monnerjahnl0 <s6lumonn@uni-bonn.de>
2024-04-22 10:46:12 +0200

    Change lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -13,5 +13,5 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib «Ohne Titel» where
+lean_lib «Construction» where
   -- add any library configuration options here

1170f089b0007d6581871ba401cf8dffaed5c97a
monnerjahnl0 <s6lumonn@uni-bonn.de>
2024-06-08 21:26:55 +0200

    Field M_inf

---
+++ Construction/Chapter2/MField.lean
@@ -0,0 +1,11 @@
+import Construction.Chapter1.basic_construction
+
+noncomputable def MField (M: Set ℂ)(h₀: 0 ∈ M)(h₁: 1∈ M): Subfield ℂ := {
+  carrier := M_inf M
+  zero_mem' := by exact M_M_inf M h₀
+  one_mem' := by exact M_M_inf M h₁
+  add_mem' := by apply add_M_Inf M h₀
+  neg_mem' := by apply z_neg_M_inf M h₀
+  mul_mem' := by apply mul_M_inf M h₀ h₁
+  inv_mem' := by apply inv_M_inf M h₀ h₁
+}

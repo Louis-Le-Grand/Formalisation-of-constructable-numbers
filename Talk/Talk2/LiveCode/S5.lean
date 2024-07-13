fd6803a77e0d80aa81f06bd8fc7da6630ce270fc
monnerjahnl0 <s6lumonn@uni-bonn.de>
2024-07-13 18:48:52 +0200

    improve handout

--- Talk/Talk2/LiveCode/S5.lean
+++ Talk/Talk2/LiveCode/S5.lean
@@ -10,29 +10,25 @@ The proof is stuctur as follows:
 open Construction
 lemma add_M_Inf (M: Set ℂ) (h₀: (0:ℂ)∈ M) (z₁ z₂ : ℂ) (hz₁ : z₁ ∈ (M_inf M)) (hz₂ : z₂ ∈ (M_inf M)):
      z₁ + z₂ ∈ (M_inf M) := by
-  by_cases h: (z₁ = z₂)
-  . by_cases hz₁0: (z₁ = 0)
-    . simp only [hz₁0, zero_add, hz₂]
-    . let c: Construction.circle := {c := z₁, r := (dist 0 z₁)}
-      let l: line := {z₁ := z₁, z₂ := 0}
-      have hc : c ∈ C (M_inf M) := by
-        refine ⟨z₁, 0, z₁, ?_, hz₁, M_M_inf M h₀, hz₁⟩
-        simp [c]
-      have hl : l ∈ L (M_inf M) := by
-        refine ⟨z₁, 0, ?_, hz₁, M_M_inf M h₀, hz₁0⟩
-        simp [l, hz₁0]
-      apply ilc_M_inf M
-      refine ⟨c, hc, l, hl, ⟨?_, ⟨2,?_⟩⟩⟩
-      . simp [circle.points, h]
-      . simp [h, two_mul]
   let c₁ : Construction.circle := {c := z₁, r := (dist 0 z₂)}
   let c₂ : Construction.circle := {c := z₂, r := (dist 0 z₁)}
+  let l: line := {z₁ := z₁, z₂ := 0}
   have hc₁ : c₁ ∈ C (M_inf M) := by
     refine ⟨z₁, 0, z₂, ?_, hz₁,M_M_inf M h₀, hz₂⟩
     simp [c₁]
   have hc₂ : c₂ ∈ C (M_inf M) := by
     refine ⟨z₂, 0, z₁, ?_,hz₂,M_M_inf M h₀,hz₁⟩
     simp [c₂]
+  by_cases h: (z₁ = z₂)
+  . by_cases hz₁0: (z₁ = 0)
+    . simp only [hz₁0, zero_add, hz₂]
+    . have hl : l ∈ L (M_inf M) := by
+        refine ⟨z₁, 0, ?_, hz₁, M_M_inf M h₀, hz₁0⟩
+        simp [l, hz₁0]
+      apply ilc_M_inf M
+      refine ⟨c₁, hc₁, l, hl, ⟨?_, ⟨2,?_⟩⟩⟩
+      . simp [circle.points, h]
+      . simp [h, two_mul]
   refine icc_M_inf M ⟨c₁,hc₁, c₂,hc₂, ?_⟩
   simp [circle.points, Set.mem_inter_iff]
   exact circle_not_eq_iff  (by exact h)

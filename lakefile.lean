434e43ac42de5e2116400596f4f48cc828aa959f
monnerjahnl0 <s6lumonn@uni-bonn.de>
2024-08-31 12:41:18 +0200

    Remake blueprint

--- lakefile.lean
+++ lakefile.lean
@@ -21,3 +21,9 @@ require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
 meta if get_config? env = some "dev" then
 require «doc-gen4» from git
   "https://github.com/leanprover/doc-gen4" @ "main"
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
